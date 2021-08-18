91748cbfa4ce97e220fac5559cb14841f6c6c257
Egor <itpd2010@yandex.ru>
2021-08-18 22:44:36 +0300

    feat: tests passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let nStr = String(Math.abs(n));
+  let result = ''       // empty string
+  let i = 0;        
+
+  while (i < nStr.length) {
+      result = `${nStr[i]}${result}`
+      i += 1
+  } 
+  return Number(`${result}`);
 }
